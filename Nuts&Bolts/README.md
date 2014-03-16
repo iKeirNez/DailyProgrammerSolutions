9e89ad7f67a612b7e03fa7149f65a66eed58dd2f
Keir Nellyer <keir@nellyer.co.uk>
2014-03-16 08:25:39 +0000

    Nuts & Bolts solution

---
+++ Nuts&Bolts/README.md
@@ -0,0 +1,6 @@
+Nuts & Bolts
+============
+
+I tried to accomplish this task in the smallest amount of code
+
+http://www.reddit.com/r/dailyprogrammer/comments/1sob1e/121113_challenge_144_easy_nuts_bolts/
---
+++ Nuts&Bolts/src/Main.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args){
+        new Main();
+    }
+
+    public Map<String, AbstractMap.SimpleEntry<Integer, Integer>> values = new HashMap<>();
+    public int rows;
+
+    public Main(){
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            rows = Integer.parseInt(reader.readLine());
+
+            for (int i = 0; i < rows * 2; i++){
+                String line = reader.readLine();
+                String[] data = line.split(" ");
+
+                if (i < rows){ // read old values
+                    values.put(data[0], new AbstractMap.SimpleEntry<>(Integer.parseInt(data[1]), 0));
+                } else { // read new values
+                    AbstractMap.SimpleEntry<Integer, Integer> existing = values.get(data[0]);
+                    existing.setValue(Integer.parseInt(data[1]));
+                    values.put(data[0], existing);
+                }
+            }
+
+            for (String item : values.keySet()){ // print prices that need changed
+                AbstractMap.SimpleEntry<Integer, Integer> prices = values.get(item);
+                int difference = prices.getValue() - prices.getKey();
+
+                if (difference != 0){
+                    System.out.println(item + " " + (difference > 0 ? "+" : "") + difference);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
