f4154b2351ef02754efc944a7e0dd3304d0a46d3
Keir Nellyer <keir@nellyer.co.uk>
2014-03-16 16:56:30 +0000

    Braille challenge solution

---
+++ Braille/README.md
@@ -0,0 +1,6 @@
+Braille
+========
+
+Simple Braille (in string form) to English converter
+
+http://www.reddit.com/r/dailyprogrammer/comments/1s061q/120313_challenge_143_easy_braille/
---
+++ Braille/src/Main.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by iKeirNez on 16/03/2014.
+ */
+public class Main {
+
+    public static Map<String, Character> dictionary = Collections.unmodifiableMap(new HashMap<String, Character>(){{
+        put("O.....", 'a');
+        put("O.O...", 'b');
+        put("OO....", 'c');
+        put("OO.O..", 'd');
+        put("O..O..", 'e');
+        put("OOO...", 'f');
+        put("OOOO..", 'g');
+        put("O.OO..", 'h');
+        put(".OO...", 'i');
+        put(".OOO..", 'j');
+        put("O...O.", 'k');
+        put("O.O.O.", 'l');
+        put("OO..O.", 'm');
+        put("OO.OO.", 'n');
+        put("O..OO.", 'o');
+        put("OOO.O.", 'p');
+        put("OOOOO.", 'q');
+        put("O.OOO.", 'r');
+        put(".OO.O.", 's');
+        put(".OOOO.", 't');
+        put("O...OO", 'u');
+        put("O.O.OO", 'v');
+        put(".OOO.O", 'w');
+        put("OO..OO", 'x');
+        put("OO.OOO", 'y');
+        put("O..OOO", 'z');
+    }});
+
+    public static String[] rows = new String[3];
+
+    public static void main(String[] args){
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            for (int i = 0; i < 3; i++){
+                rows[i] = bufferedReader.readLine();
+            }
+
+            if (!(rows[0].length() == rows[1].length() || rows[1].length() == rows[2].length())){
+                throw new RuntimeException("Inconsistent character count");
+            }
+
+            String[][] rowsData = {rows[0].split(" "), rows[1].split(" "), rows[2].split(" ")};
+            int charCount = rowsData[0].length;
+
+            String output = "";
+
+            for (int i = 0; i < charCount; i++){
+                String braille = rowsData[0][i] + rowsData[1][i] + rowsData[2][i];
+
+                if (braille.chars().parallel().filter(c -> c != ' ' && c != 'O' && c != '.').sum() > 0){ // lambda coolness to check string doesn't contain incorrect chars
+                    throw new RuntimeException("Input contains invalid characters, input must only consist of Os, .s & spaces");
+                }
+
+                if (braille.length() != 6){
+                    throw new RuntimeException("Input " + braille + " is not the correct length (!= 6)");
+                }
+
+                char englishChar = dictionary.get(braille);
+                output += englishChar;
+            }
+
+            System.out.println(output);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
